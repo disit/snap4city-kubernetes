ceda1dadfca17883e60aaf3b73854e28d4c504e0
Ubuntu <ironstonevmmaster@cvgsolvm01.dijoiuotvcqudbpupvbii1ccth.oslx.internal.cloudapp.net>
2022-01-03 14:41:00 +0000

    Initial commit

---
+++ depl-svc/setup_dir.sh
@@ -0,0 +1,5 @@
+mkdir -p /mnt/my_nfs_volumes/snap4city-docker/DataCity-Small/img
+chmod 777 /mnt/my_nfs_volumes/snap4city-docker/DataCity-Small/img
+mkdir -p /mnt/my_nfs_volumes/snap4city-docker/DataCity-Small/datamanager-logs
+chmod 777 /mnt/my_nfs_volumes/snap4city-docker/DataCity-Small/datamanager-logs
+
